2f8652886cbf348113a02b47ac0e4e501622274e
OmarAlmatar <90462587+OmarAlmatar@users.noreply.github.com>
2021-09-29 19:38:18 -0700

    Update pot.cpp

--- src/pot.cpp
+++ src/pot.cpp
@@ -2,16 +2,22 @@
 
 // Writes to digital outputs to control potentiometer relays
 void POT::set_CCW(){
-    digitalWrite(CCW, );
-    digitalWrite(MID, );
-    digitalWrite(CW, );
+    digitalWrite(CCW,1);
+    digitalWrite(MID,0);
+    digitalWrite(CW, 0);
     state = "CCW";
 }
 
 void POT::set_MID(){
+    digitalWrite(CCW,1);
+    digitalWrite(MID,1);
+    digitalWrite(CW, 0);
     state = "MID";
 }
 
 void POT::set_CW(){
+    digitalWrite(CCW,1);
+    digitalWrite(MID,1);
+    digitalWrite(CW, 1);
     state = "CW";
 }
