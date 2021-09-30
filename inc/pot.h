9f3c46c3e91a34e132c282027353c11aea11100b
OmarAlmatar <90462587+OmarAlmatar@users.noreply.github.com>
2021-09-29 19:06:52 -0700

    Update pot.h

--- inc/pot.h
+++ inc/pot.h
@@ -11,6 +11,10 @@ class POT{
         void set_CCW();
         void set_MID();
         void set_CW();
+        int get_CCW();       //to be called in main
+        int get_MID();       //to be called in main 
+        int get_CW();        //to be called in main
+        String get_state();  //to be called in main
 };
 
 #endif
