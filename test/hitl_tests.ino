9b4c130d98e49a57a63b49ee06b807e379f88072
OmarAlmatar <90462587+OmarAlmatar@users.noreply.github.com>
2021-09-29 22:54:59 -0700

    Update hitl_tests.ino

--- test/hitl_tests.ino
+++ test/hitl_tests.ino
@@ -9,13 +9,20 @@ void setup() {
 }
 
 void loop() {
-;
+  
 }
+
 
 /* Potentiometer Test
 Intantiate all relay potentiometers and cycle through different values
 */
 void test_pots() {
+  POT p1;       //object instantiated from class POT
+  p1.set_CCW();
+  
+  p1.set_MID();
+  
+  p1.set_CW();
 
   Serial.print("Pot Test Complete");
 }
